d3031fda2ad47012e7a0976aa1511329ff25bb9e
pentius00 <pentius00@gmail.com>
2023-05-15 19:10:08 -0400

    added files

---
+++ static/chatbot.js
@@ -0,0 +1,6 @@
+function toggleChatbot() {
+    var chatbotWidget = document.getElementById('chatbot-widget');
+    chatbotWidget.classList.toggle('open');
+}
+
+// Additional chatbot functionality can be added here
