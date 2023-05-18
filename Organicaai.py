d096c853fc1f0dbf5e9a044a6c4b49730b1185fa
Diego Torres <91633094+pentius00@users.noreply.github.com>
2023-05-18 17:24:40 -0400

    Update Organicaai.py

--- Organicaai.py
+++ Organicaai.py
@@ -177,7 +177,9 @@ def generate_inquiry_response(conversation):
 
     reply = response.choices[0].text.strip()
     return reply
-if __name__ == '__main__':
-    create_table()
-    app.run(debug=True)
 
+
+if __name__ == '__main__':
+    # Use the PORT environment variable provided by Heroku
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
