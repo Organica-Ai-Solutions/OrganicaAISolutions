b208056f21b986ea4aec1db537658b0c7e42e875
Diego Torres <pentius00@gmail.com>
2025-06-09 00:31:10 -0400

    Fix: Jest and Babel configuration issues - Renamed config files to .cjs for ES module compatibility - Fixed babel and jest configs to use CommonJS syntax - Note: Tests still have import.meta.env issues that need Vite test runner

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,4 +1,4 @@
-export default {
+module.exports = {
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['@babel/preset-env', '@babel/preset-react'] }],
